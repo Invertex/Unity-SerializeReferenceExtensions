4ebe8954319809d6c361a19195f2672accf9b17f
Dabbles in too many things <mitch.zais@gmail.com>
2023-05-11 21:08:37 -0700

    CustomPropertyDrawer support
    Added support for drawing the CustomPropertyDrawer of a SerializedReference if one exists.

--- Assets/MackySoft/MackySoft.SerializeReferenceExtensions/Editor/SubclassSelectorDrawer.cs
+++ Assets/MackySoft/MackySoft.SerializeReferenceExtensions/Editor/SubclassSelectorDrawer.cs
@@ -46,14 +46,63 @@ namespace MackySoft.SerializeReferenceExtensions.Editor {
 					popup.TypePopup.Show(popupPosition);
 				}
 
-				// Draw the managed reference property.
-				EditorGUI.PropertyField(position,property,label,true);
+				// Try to draw the managed reference property with a custom PropertyDrawer if it has one, otherwise draw default property field.
+				if (property.managedReferenceValue == null || !TryDrawCustomUI(position, property, label)) {
+					// Draw the managed reference property.
+					EditorGUI.PropertyField(position, property, label, true); 
+				} 
 			} else {
 				EditorGUI.LabelField(position,label,k_IsNotManagedReferenceLabel);
 			}
 
 			EditorGUI.EndProperty();
 		}
+		
+		/// <summary>
+		/// If target SerializedReference type has a CustomPropertyDrawer defined, we will draw it instead of the default style.
+		/// </summary>
+		/// <param name="position"></param>
+		/// <param name="property"></param>
+		/// <param name="label"></param>
+		/// <returns></returns>
+		private bool TryDrawCustomUI(Rect position, SerializedProperty property, GUIContent label)
+        	{
+			var scriptAttrUtility = typeof(PropertyDrawer).Assembly.GetType("UnityEditor.ScriptAttributeUtility");
+			//Returns the actual CustomPropertyDrawer that might exist for the actual reference, instead of the SubclassSelectorDrawer context we're in.
+			var getDrawerType = scriptAttrUtility.GetMethod("GetDrawerTypeForPropertyAndType", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+			var customDrawerType = (Type)getDrawerType.Invoke(null, new object[] { property, property.managedReferenceValue.GetType() });
+			//Doesn't have a CustomPropertyDrawer
+			if(customDrawerType == null) { return false; }
+			//Get the Property Drawer instance manager for this type
+			var getPropertyHandler = scriptAttrUtility.GetMethod("GetHandler", BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+			var propertyHandler = getPropertyHandler.Invoke(null, new object[] { property });
+
+			var getDrawerList = propertyHandler.GetType().GetField("m_PropertyDrawers", BindingFlags.NonPublic | BindingFlags.Instance);
+			List<PropertyDrawer> drawerList = (List<PropertyDrawer>)getDrawerList.GetValue(propertyHandler);
+
+			var drawerCount = drawerList.Count;
+			var nestingLevel = (int)propertyHandler.GetType().GetField("m_NestingLevel", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(propertyHandler);
+
+			//If our drawerCount is the same as our nesting level, it means we haven't created an instance of our CustomPropertyDrawer yet as there are less Drawers than NestingLevels.
+			if(drawerCount > 0 && drawerCount == nestingLevel)
+            		{
+				//Get the FieldInfo of the field that holds the SerializedReference of this.
+				var parentType = property.serializedObject.targetObject.GetType();
+				var targetField = parentType.GetField(property.propertyPath, BindingFlags.Instance | BindingFlags.Public | BindingFlags.Public);
+
+				PropertyDrawer propertyDrawer = (PropertyDrawer)Activator.CreateInstance(customDrawerType);
+				//Let the propertyDrawer know what field it's operating on
+				propertyDrawer.GetType().GetField("m_FieldInfo", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(propertyDrawer, targetField);
+
+				drawerList.Add(propertyDrawer);
+			}
+
+			var customDrawer = drawerList[nestingLevel];
+			//Finally trigger our CustomPropertyDrawer to draw itself.
+			customDrawer.OnGUI(position, property, label);
+
+			return true;
+        	}
 
 		TypePopupCache GetTypePopup (SerializedProperty property) {
 			// Cache this string. This property internally call Assembly.GetName, which result in a large allocation.
